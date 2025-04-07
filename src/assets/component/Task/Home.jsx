d7438534109082198fe9d4bc29bf234e968d47a9
amruta rajmane <amrutarajmane.in@gmail.com>
2025-04-07 20:16:49 +0530

    delete p

--- src/assets/component/Task/Home.jsx
+++ src/assets/component/Task/Home.jsx
@@ -5,8 +5,7 @@ const Home=()=>{
     <>
       
       <h1>Home</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, commodi.</p>
-
+      
       <h5>
         {/* Replace placeholder text with meaningful content */}
         Welcome to the Home page! Stay tuned for updates and announcements.
